723d0b82169e7ef605ab001ee3b7e79a36850658
RohitPalod <palodrht@gmail.com>
2017-09-19 17:11:49 +0530

    Initial commit -> Added gitignore

---
+++ src/main/java/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * Created by PalodRohit on 9/19/2017.
+ */
+public class Demo {
+    public static void main(String [] args ){
+        Student student =  new Student();
+        student.setCollegeId("12IT44");
+        student.setName("Rohit");
+        System.out.println("My name is "+ student.getName() + "and college Id is "+ student.getCollegeId());
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,24 @@
+/**
+ * Created by PalodRohit on 9/19/2017.
+ */
+public class Student {
+
+    private String name ;
+    private String collegeId;
+
+    public void setCollegeId(String collegeId) {
+        this.collegeId = collegeId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCollegeId() {
+        return collegeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
